636ed4255b63a2eae948c25c32af16b96ddc28c3
echooops <echooops@outlook.com>
2019-04-03 18:41:16 +0800

    新增增删文件/目录功能

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -9,6 +9,7 @@
 
 #include <fuse/fuse.h>
 
+#include <sys/types.h>
 #include <sys/xattr.h>
 
 #include <unistd.h>             // rmdir
@@ -37,44 +38,119 @@ void to_json(nlohmann::json &j, const struct fuse_context &context) {
          {"umask", context.umask}};
 }
 
+void to_json(nlohmann::json& j, const struct stat& stbuf) {
+    j = nlohmann::json{{"st_mode", stbuf.st_mode},
+                       {"st_ino", stbuf.st_ino},
+                       {"st_dev", stbuf.st_dev},
+                       {"st_rdev", stbuf.st_rdev},
+                       {"st_nlink", stbuf.st_nlink},
+                       {"st_uid", stbuf.st_uid},
+                       {"st_gid", stbuf.st_gid},
+                       {"st_size", stbuf.st_size},
+                       {"st_atime", stbuf.st_atime},
+                       {"st_mtime", stbuf.st_mtime},
+                       {"st_ctime", stbuf.st_ctime},
+                       {"st_blksize", stbuf.st_blksize},
+                       {"st_blocks", stbuf.st_blocks}};
+}
+
 void from_json(const nlohmann::json& j, struct stat &stbuf) {
+    j.at("st_mode").get_to(stbuf.st_mode);
+    j.at("st_ino").get_to(stbuf.st_ino);
+    j.at("st_dev").get_to(stbuf.st_dev);
+    j.at("st_rdev").get_to(stbuf.st_rdev);
+    j.at("st_nlink").get_to(stbuf.st_nlink);
     j.at("st_uid").get_to(stbuf.st_uid);
     j.at("st_gid").get_to(stbuf.st_gid);
-    j.at("st_mode").get_to(stbuf.st_mode);
-    j.at("st_nlink").get_to(stbuf.st_nlink);
     j.at("st_size").get_to(stbuf.st_size);
+    j.at("st_atime").get_to(stbuf.st_atime);
+    j.at("st_mtime").get_to(stbuf.st_mtime);
+    j.at("st_ctime").get_to(stbuf.st_ctime);
+    j.at("st_blksize").get_to(stbuf.st_blksize);
+    j.at("st_blocks").get_to(stbuf.st_blocks);
 }
+
+int efs_access(const char *path, int mask)
+{
+    try {
+        nlohmann::json req = { {"method", "access"},
+                               {"context", *fuse_get_context()},
+                               {"args", {{"path", path},
+                                         {"mask", mask}}}};
+        LDEBUG("efs_access {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+        auto rep = global::client.request(req);
+        LDEBUG("efs_access -------------------------------------------- rep \n{}", rep.dump().c_str());
+        return rep["ret"];
+    } catch (std::exception &e) {
+        LERROR("{}", e.what());
+        return -1;
+    }
+
+}
+#ifdef __APPLE__
+int efs_setxattr(const char *path, const char *name, const char *value, size_t size, int flags, uint32_t position)
+{
+    return -ENOTSUP;
+}
+int efs_getxattr(const char *path, const char *name, char *value, size_t size, uint32_t position)
+{
+    return -ENOTSUP;
+}
+#else /* !__APPLE__ */
+int efs_setxattr(const char *path, const char *name, const char *value, size_t size, int flags)
+{
+    return -ENOTSUP;
+}
+int efs_getxattr(const char *path, const char *name, char *value, size_t size)
+{
+    return -ENOTSUP;
+}
+#endif /* __APPLE__ */
+
+int efs_listxattr(const char *path, char *list, size_t size)
+{
+    return -ENOTSUP;
+}
+int efs_removexattr(const char *path, const char *name)
+{
+    return -ENOTSUP;
+}
+
 
 // 获取节点属性
 int efs_getattr(const char *path, struct stat *stbuf)
 {
     nlohmann::json req = { {"method", "getattr"},
                            {"context", *fuse_get_context()},
-                           {"args", {{"path", path}}}};
-
-    // LDEBUG("efs_getattr -------------------------------------------- req \n{}", req.dump().c_str());
-    auto rep = global::client.request(req);
-    // LDEBUG("efs_getattr -------------------------------------------- rep \n{}", rep.dump().c_str());
-    int ret = rep["ret"];
-    if (ret == 0)
+                           {"args", {{"path", path},
+                                     {"stat", *stbuf}}}};
+    try {
+        LDEBUG("efs_getattr {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+        auto rep = global::client.request(req);
+        LDEBUG("efs_getattr -------------------------------------------- rep \n{}", rep.dump().c_str());
         *stbuf = rep["stat"];
-    return ret;
+        return rep["ret"];
+    } catch (std::exception &e) {
+        LERROR("{}", e.what());
+        return -1;
+    }
 }
 
 int efs_mkdir(const char *path, mode_t mode)
 {
     nlohmann::json req = { {"method", "mkdir"},
                            {"context", *fuse_get_context()},
-                           {"args", {{"path", path}}}};
-    
-    LDEBUG("efs_mkdir -------------------------------------------- req \n{}", req.dump().c_str());
-
-    return mkdir(path, mode);
+                           {"args", {{"path", path},
+                                     {"mode", mode}}}};
+    LDEBUG("efs_mkdir {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    auto rep = global::client.request(req);
+    LDEBUG("efs_mkdir -------------------------------------------- rep \n{}", rep.dump().c_str());
+    return rep["ret"];
 }
 
 int efs_opendir(const char *path, struct fuse_file_info *fi)
 {
-    LDEBUG("efs_opendir -------------------------------------------- req \n{}", path);
+    LDEBUG("efs_opendir {} -------------------------------------------- req \n", path);
     return 0;
 }
 
@@ -84,9 +160,9 @@ int efs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offse
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_readdir -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_readdir {} ----------------------------------------- req \n{}", path, req.dump().c_str());
     auto rep = global::client.request(req);
-    LDEBUG("efs_readdir -------------------------------------------- req \n{}", rep.dump().c_str());
+    LDEBUG("efs_readdir -------------------------------------------- rep \n{}", rep.dump().c_str());
     for (auto &e : rep["node"]) {
         filler(buf, e.get<std::string>().c_str(), nullptr, 0);
     }
@@ -101,9 +177,11 @@ int efs_rmdir(const char *path)
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_rmdir -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_rmdir {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    auto rep = global::client.request(req);
+    LDEBUG("efs_readdir -------------------------------------------- rep \n{}", rep.dump().c_str());
 
-    return rmdir(path);
+    return rep["ret"];
 }
 
 int efs_releasedir(const char *path, fuse_file_info *fi)
@@ -112,7 +190,7 @@ int efs_releasedir(const char *path, fuse_file_info *fi)
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_releasedir -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_releasedir {} ----------------------------------------- req \n{}", path, req.dump().c_str());
 
     return 0;
 }
@@ -124,7 +202,18 @@ int efs_open(const char *path, struct fuse_file_info *fi)
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_open -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_open {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+
+    return 0;
+}
+int efs_release(const char *path, struct fuse_file_info *fi)
+{
+
+    nlohmann::json req = { {"method", "release"},
+                           {"context", *fuse_get_context()},
+                           {"args", {{"path", path}}}};
+    
+    LDEBUG("efs_release {} ----------------------------------------- req \n{}", path, req.dump().c_str());
 
     return 0;
 }
@@ -135,7 +224,7 @@ int efs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_read -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_read {} ----------------------------------------- req \n{}", path, req.dump().c_str());
 
     return 0;
 }
@@ -146,7 +235,7 @@ int efs_write(const char *path, const char *buf, size_t size, off_t offset, stru
                            {"context", *fuse_get_context()},
                            {"args", {{"path", path}}}};
     
-    LDEBUG("efs_write -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_write {} ----------------------------------------- req \n{}", path, req.dump().c_str());
 
     return 0;
 }
@@ -158,12 +247,19 @@ int efs_mknod(const char *path, mode_t mode, dev_t rdev)
                                      {"mode", mode},
                                      {"rdev", rdev}}}};
     
-    LDEBUG("efs_mknod -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_mknod {} ----------------------------------------- req \n{}", path, req.dump().c_str());
     return 0;
 }
-int efs_unlink()
+int efs_unlink(const char *path)
 {
-    
+    nlohmann::json req = { {"method", "unlink"},
+                           {"context", *fuse_get_context()},
+                           {"args", {{"path", path}}} };
+    LDEBUG("efs_unlink {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    auto rep = global::client.request(req);
+    LDEBUG("efs_unlink  ------------------------------------------- req \n{}", rep.dump().c_str());
+
+    return rep["ret"];
 }
 
 int efs_chmod(const char *path, mode_t mode)
@@ -173,11 +269,42 @@ int efs_chmod(const char *path, mode_t mode)
                            {"args", {{"path", path},
                                      {"mode", mode}}} };
     
-    LDEBUG("efs_chmod -------------------------------------------- req \n{}", req.dump().c_str());
+    LDEBUG("efs_chmod {} ----------------------------------------- req \n{}", path, req.dump().c_str());
     auto rep = global::client.request(req);
-    LDEBUG("efs_chmod -------------------------------------------- req \n{}", rep.dump().c_str());
+    LDEBUG("efs_chmod -------------------------------------------- rep \n{}", rep.dump().c_str());
     return rep["ret"];
 }
+
+int efs_statfs(const char *path, struct statvfs *buf)
+{
+    nlohmann::json req = { {"method", "chmod"},
+                           {"context", *fuse_get_context()},
+                           {"args", {{"path", path}}} };
+    
+    LDEBUG("efs_statfs {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    return 0;
+}
+int efs_readlink(const char *path, char *buf, size_t len)
+{
+    nlohmann::json req = { {"method", "chmod"},
+                           {"context", *fuse_get_context()},
+                           {"args", {{"path", path},
+                                     {"buf", buf},
+                                     {"len", len}}} };
+    LDEBUG("efs_readlink {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    return 0;
+}
+int efs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+    nlohmann::json req = { {"method", "create"},
+                           {"context", *fuse_get_context()},
+                           {"args", {{"path", path}, {"mode", mode}}}};
+    LDEBUG("efs_create {} ----------------------------------------- req \n{}", path, req.dump().c_str());
+    auto rep = global::client.request(req);
+    LDEBUG("efs_create -------------------------------------------- rep \n{}", rep.dump().c_str());
+    return rep["ret"];
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -185,9 +312,9 @@ int main(int argc, char *argv[])
     memset(&fake_ops, 0, sizeof(struct fuse_operations));
 
     fake_ops.getattr = efs_getattr;
-    fake_ops.readlink = nullptr;
+    fake_ops.readlink = efs_readlink;
     fake_ops.mknod = efs_mknod;
-    fake_ops.unlink = nullptr;
+    fake_ops.unlink = efs_unlink;
     fake_ops.symlink = nullptr;
     fake_ops.rename = nullptr;
     fake_ops.link = nullptr;
@@ -198,17 +325,17 @@ int main(int argc, char *argv[])
 
     fake_ops.open = efs_open;
     fake_ops.read = efs_read;
-    fake_ops.release = nullptr;
+    fake_ops.release = efs_release;
     fake_ops.write = efs_write;
 
-    fake_ops.statfs = nullptr;
+    fake_ops.statfs = efs_statfs;
     fake_ops.flush = nullptr;
     fake_ops.fsync = nullptr;
 
-    fake_ops.setxattr = nullptr;
-    fake_ops.getxattr = nullptr;
-    fake_ops.listxattr = nullptr;
-    fake_ops.removexattr = nullptr;
+    fake_ops.setxattr = efs_setxattr;
+    fake_ops.getxattr = efs_getxattr;
+    fake_ops.listxattr = efs_listxattr;
+    fake_ops.removexattr = efs_removexattr;
 
     fake_ops.mkdir = efs_mkdir;
     fake_ops.rmdir = efs_rmdir;
@@ -220,8 +347,8 @@ int main(int argc, char *argv[])
 
     fake_ops.init = nullptr;
     fake_ops.destroy = nullptr;
-    fake_ops.access = nullptr;
-    fake_ops.create = nullptr;
+    fake_ops.access = efs_access;
+    fake_ops.create = efs_create;
     fake_ops.ftruncate = nullptr;
     fake_ops.fgetattr = nullptr;
  
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -16,11 +16,34 @@ namespace global {
 }  // global
 
 void to_json(nlohmann::json& j, const struct stat& stbuf) {
-    j = nlohmann::json{{"st_uid", stbuf.st_uid},
-                       {"st_gid", stbuf.st_gid},
-                       {"st_mode", stbuf.st_mode},
+    j = nlohmann::json{{"st_mode", stbuf.st_mode},
+                       {"st_ino", stbuf.st_ino},
+                       {"st_dev", stbuf.st_dev},
+                       {"st_rdev", stbuf.st_rdev},
                        {"st_nlink", stbuf.st_nlink},
-                       {"st_size", stbuf.st_size}};
+                       {"st_uid", stbuf.st_uid},
+                       {"st_gid", stbuf.st_gid},
+                       {"st_size", stbuf.st_size},
+                       {"st_atime", stbuf.st_atime},
+                       {"st_mtime", stbuf.st_mtime},
+                       {"st_ctime", stbuf.st_ctime},
+                       {"st_blksize", stbuf.st_blksize},
+                       {"st_blocks", stbuf.st_blocks}};
+}
+void from_json(const nlohmann::json& j, struct stat &stbuf) {
+    j.at("st_mode").get_to(stbuf.st_mode);
+    j.at("st_ino").get_to(stbuf.st_ino);
+    j.at("st_dev").get_to(stbuf.st_dev);
+    j.at("st_rdev").get_to(stbuf.st_rdev);
+    j.at("st_nlink").get_to(stbuf.st_nlink);
+    j.at("st_uid").get_to(stbuf.st_uid);
+    j.at("st_gid").get_to(stbuf.st_gid);
+    j.at("st_size").get_to(stbuf.st_size);
+    j.at("st_atime").get_to(stbuf.st_atime);
+    j.at("st_mtime").get_to(stbuf.st_mtime);
+    j.at("st_ctime").get_to(stbuf.st_ctime);
+    j.at("st_blksize").get_to(stbuf.st_blksize);
+    j.at("st_blocks").get_to(stbuf.st_blocks);
 }
 
 struct node_t {
@@ -31,23 +54,36 @@ struct node_t {
     std::vector<struct node_t> subs;
 };
 
+nlohmann::json rxc_access(const nlohmann::json &req)
+{
+    nlohmann::json rep = {{"ret", 0}};
+
+    return rep;
+}
+
 nlohmann::json rxc_getattr(const nlohmann::json &req)
 {
     std::string path = req["args"]["path"];
 
-    struct stat st;
-    // int ret = lstat("/", &st);
+    // path = "." + path;
+    
+    // LDEBUG("看看权限 {}", path.c_str());
+    // struct stat st = req["args"]["stat"];
+    
+    // int ret = lstat(path.c_str(), &st);
     //  nlohmann::json j = st;
     // LDEBUG("看看权限 {}", j.dump(4).c_str());
     
-    // nlohmann::json rep = { {"ret", -1},
-    //                        {"stat", st} };
-    nlohmann::json rep = {{"ret", -1},
-                          {"stat", st}};
+    nlohmann::json rep = { {"ret", -ENOENT},
+                           {"stat", req["args"]["stat"]} };
+//    return rep;
+    
+    // nlohmann::json rep = {{"ret", -1},
+    //                       {"stat", st}};
 //    rep["ret"] = 0;
     if (global::o.find(path) != global::o.end()) {
         rep["ret"] = 0;
-        rep["stat"] = global::o[path];
+        rep["stat"] = global::o[path]["stat"];
     }
     // nlohmann::json rep = { {"ret", ret},
     //                        {"stat", st} };
@@ -76,14 +112,81 @@ nlohmann::json rxc_readdir(const nlohmann::json &req)
     rep["ret"] = 0;
     return rep;
 }
+nlohmann::json rxc_mkdir(const nlohmann::json &req)
+{
+    std::string path = req["args"]["path"];
+
+    struct stat st;
+    memset(&st, 0, sizeof(struct stat));
+
+    global::o[path]["stat"] = st;
+
+    nlohmann::json &j = global::o[path]["stat"];
+    j["st_mode"] = req["args"]["mode"];
+    j["st_uid"] = req["context"]["uid"];
+    j["st_gid"] = req["context"]["gid"];
+
+    // 回写回去
+    std::ofstream o("dtree.json");
+    o << std::setw(4) << global::o << std::endl;
+
+    return {{"ret", 0}};
+}
+nlohmann::json rxc_rmdir(const nlohmann::json &req)
+{
+    std::string path = req["args"]["path"];
+
+    global::o.erase(path);
+
+    // 回写回去
+    std::ofstream o("dtree.json");
+    o << std::setw(4) << global::o << std::endl;
+
+    return {{"ret", 0}};
+}
+nlohmann::json rxc_create(const nlohmann::json &req)
+{
+    std::string path = req["args"]["path"];
+
+    struct stat st;
+    memset(&st, 0, sizeof(struct stat));
+
+    global::o[path]["stat"] = st;
+
+    nlohmann::json &j = global::o[path]["stat"];
+    j["st_mode"] = req["args"]["mode"];
+    j["st_uid"] = req["context"]["uid"];
+    j["st_gid"] = req["context"]["gid"];
+
+    // 回写回去
+    std::ofstream o("dtree.json");
+    o << std::setw(4) << global::o << std::endl;
+
+    return {{"ret", 0}};
+}
+
+nlohmann::json rxc_unlink(const nlohmann::json &req)
+{
+    std::string path = req["args"]["path"];
+
+    global::o.erase(path);
+
+    // 回写回去
+    std::ofstream o("dtree.json");
+    o << std::setw(4) << global::o << std::endl;
+
+    return {{"ret", 0}};
+}
+
+
 nlohmann::json rxc_chmod(const nlohmann::json &req)
 {
     std::string path = req["args"]["path"];
 
-    global::o[path]["st_mode"] = req["args"]["mode"];
+    global::o[path]["stat"]["st_mode"] = req["args"]["mode"];
     
+    // 回写回去
     std::ofstream o("dtree.json");
-    
     o << std::setw(4) << global::o << std::endl;
 
     return {{"ret", 0}};
@@ -91,8 +194,13 @@ nlohmann::json rxc_chmod(const nlohmann::json &req)
 
 void register_methods()
 {
+    global::server.register_methods("access", rxc_access);
     global::server.register_methods("getattr", rxc_getattr);
     global::server.register_methods("readdir", rxc_readdir);
+    global::server.register_methods("mkdir", rxc_mkdir);
+    global::server.register_methods("rmdir", rxc_rmdir);
+    global::server.register_methods("create", rxc_create);
+    global::server.register_methods("unlink", rxc_unlink);
     global::server.register_methods("chmod", rxc_chmod);
     global::server.start();
 }
